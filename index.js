bec911c138a9dcc1a3ce7b780d71ce7681ceba7d
shivamagarg <shivamagarg@gmail.com>
2022-07-18 19:27:09 +0530

    Adding Files and Configurations

---
+++ index.js
@@ -0,0 +1,34 @@
+let score1=document.getElementById("score1-el");
+let score2=document.getElementById("score2-el");
+
+let count1=0,count2=0;
+
+function plusOne(){
+    count1+=1;
+    score1.textContent=count1;
+}
+
+function plusTwo(){
+    count1+=2;
+    score1.textContent=count1;
+}
+
+function plusThree(){
+    count1+=3;
+    score1.textContent=count1;
+}
+
+function plusOne2(){
+    count2+=1;
+    score2.textContent=count2;
+}
+
+function plusTwo2(){
+    count2+=2;
+    score2.textContent=count2;
+}
+
+function plusThree2(){
+    count2+=3;
+    score2.textContent=count2;
+}
